41679cf53273ac6a77a1e7b25adbd74c76198edf
i-stam <i.stamelakos@gmail.com>
2022-09-15 19:01:45 +0300

    Add license

--- contracts/LicensedNFT.sol
+++ contracts/LicensedNFT.sol
@@ -3,9 +3,16 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {LicenseVersion, CantBeEvil} from "@a16z/contracts/licenses/CantBeEvil.sol";
 
-contract LicensedNFT is ERC721 {
 
-    constructor() ERC721("LicensedNFT","LNFT") {}
+contract LicensedNFT is ERC721("LicensedNFT","LNFT"), CantBeEvil(LicenseVersion.CBE_CC0) {
+
+    constructor() {}
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, CantBeEvil) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
 
 }
